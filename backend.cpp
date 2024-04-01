af854975d834af6b15ed59c16e32850578f52dd8
Mritunjaii <themritunjai@gmail.com>
2024-04-01 22:29:33 +0530

    bacend

---
+++ backend.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+float cal_m1(float ld, float rd)
+{
+
+    return ((2 * ld) / (M_PI * pow(rd,2)));
+}
+float cal_m2(float matl, float lenh,float angle, float rd){
+    return  matl * angle * rd / lenh;
+    
+}
+
+int main()
+{
+
+    
+    int choose;
+    cin >> choose;
+    if (choose == 1)
+    {
+        float load, radius;
+        cout << "load on cylindrical shaft";
+        cin >> load;
+        cout << "radius of cylinder";
+        cin >> radius;
+        float re = cal_m1(load, radius);
+        cout << re;
+    }
+    else if (choose == 2)
+    {
+        float matr, len, ang, radius;
+        cout << "material of cylindrical shaft";
+        cin >> matr;
+        cout << "lenght of cylindrical shaft";
+        cin >> len;
+        cout << "angle of twist cylindrical shaft";
+        cin >> ang;
+        cout << "radius of cylindrical shaft";
+        cin >> radius;
+        float re = cal_m2(matr, len, ang, radius);
+        cout<< re;
+
+
+    }
+}
